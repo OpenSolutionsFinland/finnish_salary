479048491a882201c00803bca482fa6ca897c257
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-03-03 12:49:28 +0200

    Fixing inherited class name

--- contract.py
+++ contract.py
@@ -2,8 +2,8 @@ from osv import osv, fields
 from openerp.tools.translate import _
 
 class hr_contract_finnish(osv.osv):
-    _name="hr.contract"
-    _inherit="hr.contract"
+    _name="hr_contract"
+    _inherit="hr_contract"
     
     _columns = {
         'tax_card_type': fields.selection((('A', 'A'), ('B', 'B')), _('Tax card type'), required=False),
