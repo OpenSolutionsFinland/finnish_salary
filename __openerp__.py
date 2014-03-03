848d54c0f04699a4f047959d111c211d973d7c51
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-03-03 12:51:41 +0200

    Fixing inherited class name

--- __openerp__.py
+++ __openerp__.py
@@ -27,7 +27,7 @@
     OpenERP module that adds fields to HR contracts for calculating Finnish salaries
     """,
     "website" : "http://www.opensolutions.fi",
-    "depends" : ["base","hr"],
+    "depends" : ["base","hr", "hr_contract"],
     "category" : "Generic Modules",
     "init_xml" : [],
     "demo_xml" : [],
